091fa91c0a899823ab0f499fd7c7f5bd9a88add1
Ashyraffa <raffadandilla@gmail.com>
2025-01-19 19:24:20 +0700

    improved commit

--- 8.1.js
+++ 8.1.js
@@ -1,6 +1,6 @@
 
 let gambarIndex = 0;
-let gambarArray = ["gambar1.jpg", "gambar2.jpg", "gambar3.jpg", "gambar4.jpg", "gambar5.jpg", "gambar6.jpg", "gambar7.jpg"]
+let gambarArray = ["gambar1.jpg", "gambar2.jpg", "gambar3.jpg", "gambar4.jpg", "gambar5.jpg", "gambar6.jpg", "gambar7.jpg", "gambar8.jpg", "gambar9.jpg"]
 
 function gantiGambar() {
     gambarIndex = (gambarIndex + 1) % gambarArray.length;
