a96a26d19894d26f0c876311caad2e96135651cc
Ashyraffa3527 <raffadandilla@gmail.com>
2025-02-05 17:37:43 +0700

    FIxing Bugs

--- faq.js
+++ faq.js
@@ -1,6 +1,11 @@
+// This was used to animate and make drawers for //
+// "questions.html" and "Tugas yang harus dikerjakan.html". //
+
 document.querySelectorAll('.pertanyaan').forEach(button => {
     button.addEventListener('click', () => {
         const jawab = button.nextElementSibling;
         jawab.style.display = jawab.style.display === 'block' ? 'none' : 'block';
     });
 });
+
+console.log('Okay.')
