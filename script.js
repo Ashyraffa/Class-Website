5da325a346cd0f3e92a7a0fb07e7e8bf96154acc
Ashyraffa3527 <raffadandilla@gmail.com>
2025-03-26 10:37:30 +0700

    new

--- script.js
+++ script.js
@@ -76,3 +76,19 @@ function toggleContent(contentId) {
     // Initialize the slider
     showSlide(currentIndex);
 });
+
+
+const texts = ["Do more things with 8-1", "Do unexpected things with 8-1"]
+let index = 0;
+const textElement = document.getElementById("changingText");
+
+function changeText() {
+    textElement.style.opacity = 0;
+    setTimeout(() => {
+        index = (index + 1) % texts.length;
+        textElement.textContent = texts[index];
+        textElement.style.opacity = 1; 
+    }, 500); 
+    }
+
+    setInterval(changeText, 4000);
